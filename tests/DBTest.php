46edcbc8f5c1411d0e494f098bab5e9dcb444701
root <cymapkenya@gmail.com>
2019-09-25 03:22:35 +0300

    Add type hints to inherited methods of test cases

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -17,7 +17,7 @@ class DBTest extends \PHPUnit\Framework\TestCase {
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
      */
-    protected function setUp() {
+    protected function setUp(): void {
        $dbParams = array (
             'driver'   => 'pdo_sqlite',
             'path'     => __DIR__ . '/files/db.test.sqlite'
@@ -50,7 +50,7 @@ class DBTest extends \PHPUnit\Framework\TestCase {
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
      */
-    protected function tearDown() {
+    protected function tearDown(): void {
 
     }
 
